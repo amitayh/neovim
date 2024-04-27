208c2fc8cceee80d23230f8a35b528ff1b6c3261
Amitay Horwitz <amitayh@gmail.com>
2024-04-27 10:26:47 +0300

    TreeSitter incremental selection

--- init.lua
+++ init.lua
@@ -781,6 +781,15 @@ require('lazy').setup({
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>', -- set to `false` to disable one of the mappings
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
+        },
+      },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
