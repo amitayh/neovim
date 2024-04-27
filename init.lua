7060b1f9c06736652c50fbe3fff4716a996b919d
Amitay Horwitz <amitayh@gmail.com>
2024-04-27 10:26:47 +0300

    TreeSitter incremental selection

--- init.lua
+++ init.lua
@@ -895,6 +895,15 @@ require('lazy').setup({
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>', -- set to `false` to disable one of the mappings
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
+        },
+      },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
