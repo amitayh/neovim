4288039de826cb0d78ad6c1ea08db076d63820b2
Amitay Horwitz <amitayh@gmail.com>
2024-12-10 15:47:05 +0200

    neogit

--- init.lua
+++ init.lua
@@ -912,8 +912,17 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- Diff integration
+      'nvim-telescope/telescope.nvim',
+    },
+    config = true,
+  },
 
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
+  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
 
